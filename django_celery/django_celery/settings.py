02e7f42d3fdf4b7c3af70dce9caf9641b3058fb9
Michael Florya <mih2303@mail.ru>
2024-04-14 16:12:42 +0300

    create custom users model & make migrations

--- django_celery/django_celery/settings.py
+++ django_celery/django_celery/settings.py
@@ -31,12 +31,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Встроенные настройки
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Приложения
+    'main.apps.MainConfig',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +125,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'main.User'
--- django_celery/main/models.py
+++ django_celery/main/models.py
@@ -1,3 +1,56 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
-# Create your models here.
+
+class UserAccountManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Users must have an email address')
+        user = self.model(
+            email=self.normalize_email(email), **extra_fields
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        user = self.create_user(
+            email,
+            password=password, **extra_fields
+
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=255,
+        unique=True,
+    )
+    first_name = models.CharField(verbose_name='first name', max_length=30, blank=True)
+    last_name = models.CharField(verbose_name='last name', max_length=30, blank=True)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    objects = UserAccountManager()
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        """Есть ли у пользователей конкретное разрешение?"""
+        return True
+
+    def has_module_perms(self, app_label):
+        """Есть ли у пользователя разрешения на просмотр приложения `app_label`?"""
+        return True
+
+    @property
+    def is_staff(self):
+        """Является ли пользователь сотрудником?"""
+        # Самый простой ответ: Все администраторы - это сотрудники
+        return self.is_admin
