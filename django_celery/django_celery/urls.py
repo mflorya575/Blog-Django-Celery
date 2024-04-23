f198f468e986bdd060d38c9ae7be437658993252
Michael Florya <mih2303@mail.ru>
2024-04-23 19:23:35 +0300

    create url & controller with template for the publish posts

--- django_celery/django_celery/urls.py
+++ django_celery/django_celery/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from publish.views import view_post
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('<slug:slug>/', view_post, name='view_post'),
 ]
