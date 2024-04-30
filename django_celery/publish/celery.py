1a4d3f0d11ee9e079ff0fd5d60be0fd2c1f1be3f
Michael Florya <mih2303@mail.ru>
2024-04-30 22:55:27 +0300

    modern code for celery task

--- django_celery/publish/celery.py
+++ django_celery/publish/celery.py
@@ -1,5 +1,7 @@
 import os
 from celery import Celery
+from celery.schedules import crontab
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_celery.settings')
 app = Celery('publish', broker_connection_retry=False,
@@ -8,3 +10,9 @@ app.config_from_object('django.conf:settings')
 broker_connection_retry = False
 
 app.autodiscover_tasks()
+app.conf.beat_schedule = {
+    'send-report-every-single-minute': {
+        'task': 'publish.tasks.send_view_count_report',
+        'schedule': crontab(),
+    },
+}
