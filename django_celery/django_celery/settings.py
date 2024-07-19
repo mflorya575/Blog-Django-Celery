7d4309d056ebf469d8a36585e16349e21ba5a8cd
Michael Florya <mih2303@mail.ru>
2024-07-19 17:04:44 +0300

    reconfig redis settings

--- django_celery/django_celery/settings.py
+++ django_celery/django_celery/settings.py
@@ -129,7 +129,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 REDIS_HOST = 'localhost'
-REDIS_PORT = '6380'
+REDIS_PORT = '6379'
 BROKER_URL = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0'
 BROKER_TRANSPORT_OPTIONS = {'visibility_timeout': 3600}
 CELERY_RESULT_BACKEND = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0'
